62fc72ad1a1aeec0614e0f6e28c995c99f5ef635
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-03-28 01:30:31 +0200

    formatting :+1:

--- src/alternative_models/LSTM/data_utils_lstm.py
+++ src/alternative_models/LSTM/data_utils_lstm.py
@@ -4,40 +4,42 @@ import wget
 import urllib.request
 import zipfile
 import ssl
+from sklearn.base import TransformerMixin, BaseEstimator
 
 
+def download_data(glove: str = "glove.6B.zip"):
+    if not os.path.exists(f"./{glove}"):
+        print(f"Downloading {glove}...")
+        url = f"http://nlp.stanford.edu/data/{glove}"
 
-def download_data(glove:str='glove.6B.zip'):
-
-    if not os.path.exists(f'./{glove}'):
-        print(f'Downloading {glove}...')
-        url = f'http://nlp.stanford.edu/data/{glove}'
-        
         # create a custom opener to disable SSL verification
         context = ssl.create_default_context()
         context.check_hostname = False
         context.verify_mode = ssl.CERT_NONE
-        opener = urllib.request.build_opener(urllib.request.HTTPSHandler(context=context))
-        
+        opener = urllib.request.build_opener(
+            urllib.request.HTTPSHandler(context=context)
+        )
+
         # download the file using the custom opener
         response = opener.open(url)
         data = response.read()
-        
+
         # save the downloaded data to file
-        with open(glove, 'wb') as f:
+        with open(glove, "wb") as f:
             f.write(data)
-        
-        print(f'{glove} download complete!')
+
+        print(f"{glove} download complete!")
     else:
-        print(f'{glove} already exists.')
+        print(f"{glove} already exists.")
 
     if not os.path.exists("./data"):
         os.mkdir("./data")
 
     print("Extracting...")
-    with zipfile.ZipFile('glove.6B.zip', 'r') as zip_ref:
-        zip_ref.extractall('./data')
+    with zipfile.ZipFile("glove.6B.zip", "r") as zip_ref:
+        zip_ref.extractall("./data")
     print("done!")
+
 
 def load_glove_embeddings(path):
     embeddings_index = {}
@@ -45,9 +47,10 @@ def load_glove_embeddings(path):
         for line in f:
             values = line.split()
             word = values[0]
-            coefs = np.asarray(values[1:], dtype='float32')
+            coefs = np.asarray(values[1:], dtype="float32")
             embeddings_index[word] = coefs
     return embeddings_index
+
 
 def preprocess_dataset(dataset, embeddings):
     def preprocess_sentence(sentence):
@@ -72,12 +75,25 @@ def preprocess_dataset(dataset, embeddings):
     hypothesis_embeddings = []
 
     for index, row in dataset.iterrows():
-        premise_embedding = get_embedding(row['premise'], embeddings)
-        hypothesis_embedding = get_embedding(row['hypothesis'], embeddings)
+        premise_embedding = get_embedding(row["premise"], embeddings)
+        hypothesis_embedding = get_embedding(row["hypothesis"], embeddings)
         premise_embeddings.append(premise_embedding)
         hypothesis_embeddings.append(hypothesis_embedding)
 
-    dataset['premise_embedding'] = premise_embeddings
-    dataset['hypothesis_embedding'] = hypothesis_embeddings
+    dataset["premise_embedding"] = premise_embeddings
+    dataset["hypothesis_embedding"] = hypothesis_embeddings
 
     return dataset
+
+
+class Filter(TransformerMixin, BaseEstimator):
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_filtered = X[X["label"] != -1]
+
+        return X_filtered
